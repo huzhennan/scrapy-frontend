b2cfa87306ff8c9e0a29969076c9c822eba35923
hzn <huzhennan2015@163.com>
2016-01-21 21:47:41 +0800

    添加样式

--- js/app.js
+++ js/app.js
@@ -26,17 +26,21 @@ $(document).ready(function () {
             function (data) {
                 var items = data.items;
 
-                items.forEach(function (item) {
-                    if (item.pwd) {
-                        var link_node = $("<li>链接:<a href='" + item.link + "'>" + item.link + "</a> 密码:" + item.pwd + "</li>");
-                    } else {
-                        var link_node = $("<li>链接:<a href='" + item.link + "'>" + item.link + "</a></li>");
-                    }
-                    link_node.appendTo(links);
-                    console.log(item);
-                });
-
-                msg.html("");
+                if (items.length > 0) {
+                    $('.links_title').show();
+                    items.forEach(function (item) {
+                        if (item.pwd) {
+                            var link_node = $("<li class='list-group-item'>链接:<a href='" + item.link + "'>" + item.link + "</a> 密码:" + item.pwd + "</li>");
+                        } else {
+                            var link_node = $("<li class='list-group-item'>链接:<a href='" + item.link + "'>" + item.link + "</a></li>");
+                        }
+                        link_node.appendTo(links);
+                        console.log(item);
+                    });
+                } else {
+                    $('.links_title').hide();
+                    msg.html("没有找到相关的资源");
+                }
             }
         );
     });
